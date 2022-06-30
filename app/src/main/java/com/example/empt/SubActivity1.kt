0789136910dae4cb129e5e3c186f4932b5cf2d46
JHLee <gh9439@kaist.ac.kr>
2022-06-30 22:08:36 +0900

    0630_button1_done_csv_reading

--- app/src/main/java/com/example/empt/SubActivity1.kt
+++ app/src/main/java/com/example/empt/SubActivity1.kt
@@ -2,23 +2,44 @@ package com.example.empt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ListAdapter
 import kotlinx.android.synthetic.main.activity_button1.*
-import java.io.BufferedReader
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+
 
 
 class SubActivity1 : AppCompatActivity() {
+    lateinit var PNAdapter : ListAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
-        var phonenumlist = arrayListOf<PhoneNum>(
-            PhoneNum("이지현","01090299439"),
-            PhoneNum("하현수","01012345678")
-        )
-        //try{
-            //BufferedReader br = new BufferedReader(new FileReader("phone_num_list.csv"))
-
-        //}
+        var phonenumlist = arrayListOf<PhoneNum>()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_button1)
-        val PNAdapter = PNListAdapter(this,phonenumlist)
+        try{
+            val jsonString = assets.open("phone_num_list.json")
+            val size = jsonString.available()
+            val buffer = ByteArray(size)
+            jsonString.read(buffer)
+            jsonString.close()
+            var strJson = String(buffer,Charsets.UTF_8)
+            val jsonObject = JSONObject(strJson)
+            val pnarray = jsonObject.getJSONArray("pnlist")
+            var i = 0
+            while(i<pnarray.length()){
+                val jsonObject = pnarray.getJSONObject(i)
+                val person = jsonObject.getString("name")
+                val phone_num = jsonObject.getString("phonenum")
+                val Phone_Num = PhoneNum(person,phone_num)
+                phonenumlist.add(Phone_Num)
+                i++
+            }
+        } catch (e:JSONException){
+            e.printStackTrace()
+        }
+        PNAdapter = PNListAdapter(this,phonenumlist)
         phonenum_list.adapter = PNAdapter
         backbutton1.setOnClickListener {
             finish()
